61f4f4f59f51093420501897b251e039d9061d05
kraktus <kraktus@users.noreply.github.com>
2023-01-22 17:43:01 +0100

    internal modules do not produce public facing code

--- convert.sh
+++ convert.sh
@@ -4,13 +4,11 @@ git restore src/* && \
 cargo extern-fn -vvv \
         -i board.rs \
         -i fen.rs \
-        -i magics.rs \
         -i movelist.rs \
         -i position.rs \
         -i san.rs \
         -i setup.rs \
         -i uci.rs \
-        -i util.rs \
         -i variant.rs \
         -i zobrist.rs && \
 python3 t.py && \
