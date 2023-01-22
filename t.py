84dc48c6e074e3bcafd8f2250453b283353b45d4
kraktus <kraktus@users.noreply.github.com>
2023-01-22 18:26:20 +0100

    fix bugs with monomorphising ByColor

--- t.py
+++ t.py
@@ -47,8 +47,15 @@ impl fmt::Debug for SquareFfi {
 # Classes #
 ###########
 
+
+def change_file_in_memory(file_name: str, fn):
+    with open(f"src/{file_name}.rs", "r") as f:
+        src = f.readlines()
+    with open(f"src/{file_name}.rs", "w") as f:
+        f.write(fn(src))
+
 def conversion_bc(x: str, y: str) -> str:
-    f"""impl From<{x}> for {y} {{
+    return f"""impl From<{x}> for {y} {{
     fn from(x: {x}) -> Self {{
         Self {{
             black: x.black.into(),
@@ -58,10 +65,10 @@ def conversion_bc(x: str, y: str) -> str:
 }}"""
 
 def conversion_back_and_forth_bc(x: str, y: str) -> str:
-    conversion_bc(x,y) + conversion_bc(y,x)
+    return conversion_bc(x,y) + conversion_bc(y,x)
 
 def decl_by_color(t: str) -> str:
-   decl = fr"""#[derive(Copy, Clone, Default, Eq, PartialEq, Debug, Hash)]
+   return fr"""#[derive(Copy, Clone, Default, Eq, PartialEq, Debug, Hash)]
     #[repr(C)]
     pub struct ByColor{t}Ffi {{
         pub black: {t}Ffi,
@@ -69,23 +76,29 @@ def decl_by_color(t: str) -> str:
     }}"""
 
 def monomorphise_bc(l: List[str]):
+
     for ty in l:
+        original = f"ByColor<{ty}>"
+        ffi = f"ByColor{ty}Ffi"
         insert_code("ffi", 4, decl_by_color(ty))
-        insert_code("color", 414, conversion_back_and_forth_bc(f"ByColor{t}Ffi", f"ByColor<{t}>"))
+        insert_code("color", 414, conversion_back_and_forth_bc(original, ffi))
+        def replace(src: List[str]) -> str:
+            file = "".join(src)
+            return file.replace(original, ffi)
+        change_file_in_memory("ffi", replace)
 
 def insert_code(file_name: str, line_nb: int, string: str):
-    with open(f"src/{file_name}.rs", "r") as f:
-    	src = f.readlines()
-    # lines in editor start at 1
-    src.insert(line_nb - 1, f"{string}\n")
-    with open(f"src/{file_name}.rs", "w") as f:
-    	f.write("".join(src))
+    def inner(src: str) -> str:
+        src.insert(line_nb - 1, f"{string}\n")
+        return "".join(src)
+    change_file_in_memory(file_name, inner)
 
 
 def main() -> None:
     insert_code("lib", 97, "mod ffi;")
     insert_code("bitboard", 1235, BB_DEBUG)
     insert_code("square", 968, SQUARE_DEBUG)
+    monomorphise_bc(["Bitboard"])
 
 ########
 # Main #
